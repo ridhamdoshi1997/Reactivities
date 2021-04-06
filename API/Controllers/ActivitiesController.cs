1084c8680d55d2cd7f97e154fe118aef60d98944
Ridham Doshi <ridham.doshi.rd@gmail.com>
2021-04-06 16:47:32 -0400

    End of Section 5

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,7 +18,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query() );
         }
 
         [HttpGet("{id}")]
